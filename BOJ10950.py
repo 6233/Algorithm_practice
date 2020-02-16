d60eb815cea0c5f6fbb571808f2fb878bde5b857
jhleeO <jhlee@olulo.io>
2020-02-16 13:43:45 +0900

    PYTHON

---
+++ BOJ10950.py
@@ -0,0 +1,11 @@
+#BOJ 10950
+#첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+#각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+n = int(input())
+
+for  i in range(n):
+  a ,b = input().split()
+  a = int(a)
+  b = int(b)
+  print(a+b)
